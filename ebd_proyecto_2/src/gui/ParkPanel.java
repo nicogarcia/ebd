ae4eba1ee1ae59b6ab0aeffd6deb14af59b91bba
nicoalbo90 <nicoalbo90@gmail.com>
2011-11-04 22:40:55 +0000

    Terminado proyecto 3

--- ebd_proyecto_2/src/gui/ParkPanel.java
+++ ebd_proyecto_2/src/gui/ParkPanel.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -14,19 +15,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
-import javax.swing.plaf.synth.Region;
-
-import sun.java2d.pipe.SolidTextRenderer;
-
-import com.sun.org.apache.xml.internal.serialize.LineSeparator;
 
 public class ParkPanel extends JPanel implements MouseMotionListener,
 		MouseListener {
@@ -41,7 +35,7 @@ public class ParkPanel extends JPanel implements MouseMotionListener,
 	public ParkPanel(ParkingScreen pk) {
 		super();
 		parent = pk;
-		ellipse = new Ellipse2D.Double(165, 540, 50, 50);
+		ellipse = new Ellipse2D.Double(255, 435, 50, 50);
 		imgBounds = new Rectangle2D.Double();
 
 		try {
@@ -56,7 +50,7 @@ public class ParkPanel extends JPanel implements MouseMotionListener,
 		setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
 				new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB),
 				new Point(0, 0), "name"));
-		imgBounds.setFrame(new Point(0, 0), new Dimension(img.getWidth(null),
+		imgBounds.setFrame(new Point(350 - img.getWidth(null)/2, 0), new Dimension(img.getWidth(null),
 				img.getHeight(null)));
 	}
 
@@ -66,15 +60,20 @@ public class ParkPanel extends JPanel implements MouseMotionListener,
 
 		e.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		setOpaque(false);
-		g.drawImage(img, (int) imgBounds.getX(), (int) imgBounds.getY(),
+		e.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		e.drawImage(img, (int) imgBounds.getX(), (int) imgBounds.getY(),
 				img.getWidth(null), img.getHeight(null), null);
 
 		e.setColor(Color.yellow);
 		e.setStroke(new BasicStroke(5));
-		e.draw(ellipse);
+		//e.draw(ellipse);
+		
+		e.setColor(Color.yellow);
+		e.setFont(new Font("arial", Font.BOLD, 20));
+		//e.drawString("Introduzca el cospel", 200, 520);
 
-		g.drawImage(mouse, mousePos.x - 32, mousePos.y - 32, 64, 143, null);
+		g.drawImage(mouse, mousePos.x - 32, mousePos.y - 32, 48, 108, null);
 	}
 
 	@Override
@@ -91,8 +90,13 @@ public class ParkPanel extends JPanel implements MouseMotionListener,
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		if (ellipse.contains(arg0.getPoint()))
+		if (ellipse.contains(arg0.getPoint())) {
+			setCursor(Cursor.getDefaultCursor());
 			parent.conectar();
+			setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
+					new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB),
+					new Point(0, 0), "name"));
+		}
 	}
 
 	@Override
